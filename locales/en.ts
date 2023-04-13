742cc137419c3f8fd31cf1d04f224a4afb6dc025
parsajiravand <parsajiravand@gmail.com>
2023-04-13 04:21:57 +0330

    first commit

---
+++ locales/en.ts
@@ -0,0 +1,10 @@
+export default {
+  meta: {
+    title: "Parsa Jiravand - Front-End Developer",
+    description:
+      "Hi, my name is Parsa Jiravand. I have been programming for about 4 years. I worked with private start-ups and state-owned companies.",
+  },
+  index:{
+    title:"Trader4 Base Layer"
+  }
+};
