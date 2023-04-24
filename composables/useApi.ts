5fb26f759bdb97d8b10f1d34e384d3e88b94e2f0
briofy-mammad <m.ranjbar@briofy.net>
2023-04-24 16:01:05 +0330

    add api composable

---
+++ composables/useApi.ts
@@ -0,0 +1,9 @@
+export const useApi = async (endpoint: string, payload?: any) => {
+  const baseUrl = useRuntimeConfig().public.baseUrl;
+  const { data, error } = await useFetch(baseUrl + endpoint);
+  if (data.value) {
+    return { data: data.value, error: null };
+  } else {
+    return { data: null, error: error.value };
+  }
+};
