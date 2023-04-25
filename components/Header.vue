20440f8c7c25a5f57295636125837b343091d309
briofy-mammad <m.ranjbar@briofy.net>
2023-04-25 08:30:25 +0330

    added - company mega menu and sub-nav header

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,7 @@
 <template>
   <header class="sticky z-20 top-0">
     <Announcement />
-    <nav
-      class="bg-white border-gray-200 px-4 lg:px-6 py-1 dark:bg-gray-800 shadow-md"
-    >
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-1 dark:bg-gray-800">
       <div
         class="flex flex-wrap md:justify-between justify-center items-center"
       >
@@ -75,6 +73,252 @@
           >
             {{ extraLinkItem.title }}
           </NuxtLink>
+          <button
+            id="mega-menu-button"
+            data-dropdown-toggle="mega-menu"
+            class="lg:flex hidden justify-between items-center py-2 pr-4 pl-3 w-full lg:w-auto mx-2 hover:text-blue-700 text-gray-600 dark:text-gray-300 lg:p-0"
+          >
+            Company
+            <svg
+              class="ml-1 w-5 h-5 lg:w-4 lg:h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <div
+            id="mega-menu"
+            class="grid hidden absolute z-10 w-full bg-white border border-gray-100 shadow-md dark:border-gray-700 lg:rounded-lg lg:w-auto lg:grid-cols-3 dark:bg-gray-700"
+          >
+            <div
+              class="p-2 text-gray-900 bg-white lg:rounded-lg dark:text-white lg:col-span-2 dark:bg-gray-800"
+            >
+              <ul>
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+                  >
+                    <div
+                      class="p-2 mr-4 bg-white rounded-lg shadow dark:bg-gray-700"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <div class="font-semibold">Explore Design Work</div>
+                      <div
+                        class="text-sm font-light text-gray-500 dark:text-gray-400"
+                      >
+                        Trending designs to inspire you
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+                  >
+                    <div
+                      class="p-2 mr-4 bg-white rounded-lg shadow dark:bg-gray-700"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <div class="font-semibold">New & Noteworthy</div>
+                      <div
+                        class="text-sm font-light text-gray-500 dark:text-gray-400"
+                      >
+                        Up-and-coming designers
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+                  >
+                    <div
+                      class="p-2 mr-4 bg-white rounded-lg shadow dark:bg-gray-700"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z"
+                        ></path>
+                        <path
+                          d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <div class="font-semibold">Playoffs</div>
+                      <div
+                        class="text-sm font-light text-gray-500 dark:text-gray-400"
+                      >
+                        Work designers are riffing on
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+                  >
+                    <div
+                      class="p-2 mr-4 bg-white rounded-lg shadow dark:bg-gray-700"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M5 3a1 1 0 000 2c5.523 0 10 4.477 10 10a1 1 0 102 0C17 8.373 11.627 3 5 3z"
+                        ></path>
+                        <path
+                          d="M4 9a1 1 0 011-1 7 7 0 017 7 1 1 0 11-2 0 5 5 0 00-5-5 1 1 0 01-1-1zM3 15a2 2 0 114 0 2 2 0 01-4 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <div class="font-semibold">Blog</div>
+                      <div
+                        class="text-sm font-light text-gray-500 dark:text-gray-400"
+                      >
+                        Interviews, tutorials, and more
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="flex items-center p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+                  >
+                    <div
+                      class="p-2 mr-4 bg-white rounded-lg shadow dark:bg-gray-700"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <div class="font-semibold">Weekly Warm-up</div>
+                      <div
+                        class="text-sm font-light text-gray-500 dark:text-gray-400"
+                      >
+                        Prompt to flex your skills
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div
+              class="py-5 px-5 bg-gray-50 lg:rounded-lg lg:col-span-1 dark:bg-gray-700"
+            >
+              <h3
+                class="mb-4 text-sm font-semibold text-gray-900 dark:text-white"
+              >
+                Browse categories
+              </h3>
+              <ul class="space-y-4 text-sm text-gray-500 dark:text-gray-400">
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Animation</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Branding</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Illustration</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Mobile</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Print</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Product Design</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="hover:text-primary-600 dark:hover:text-primary-500"
+                    >Web Design</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+
           <!-- <button
             type="button"
             class="hidden sm:inline-flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded text-xs px-3 py-1.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
@@ -810,6 +1054,121 @@
                   href="#"
                   class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                   >Sign out</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <nav
+      class="bg-white border-gray-200 px-4 lg:px-6 py-1 dark:bg-gray-800 shadow-md lg:block hidden"
+    >
+      <div class="py-3 px-4 mx-auto">
+        <div class="flex items-center">
+          <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+            <li>
+              <a
+                href="#"
+                class="text-gray-900 dark:text-white hover:underline"
+                aria-current="page"
+                >Home</a
+              >
+            </li>
+            <li>
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Company</a
+              >
+            </li>
+            <li>
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Team</a
+              >
+            </li>
+            <li>
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Features</a
+              >
+            </li>
+            <li class="hidden md:inline">
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Marketplace</a
+              >
+            </li>
+            <li class="hidden md:inline">
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Resources</a
+              >
+            </li>
+            <li class="hidden md:inline">
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Forum</a
+              >
+            </li>
+            <li class="hidden md:inline">
+              <a href="#" class="text-gray-900 dark:text-white hover:underline"
+                >Support</a
+              >
+            </li>
+          </ul>
+          <button
+            id="dropdownDefault"
+            data-dropdown-toggle="dropdown"
+            data-dropdown-placement="bottom"
+            class="text-gray-500 md:hidden dark:hover:bg-gray-600 dark:text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-full text-sm p-1.5"
+          >
+            <svg
+              class="w-5 h-5 text-gray-700 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"
+              ></path>
+            </svg>
+          </button>
+          <div
+            id="dropdown"
+            class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+          >
+            <ul
+              class="py-1 text-sm text-gray-700 dark:text-gray-200"
+              aria-labelledby="dropdownDefault"
+            >
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Marketplace</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Dashboard</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Resources</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Forum</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Support</a
                 >
               </li>
             </ul>
