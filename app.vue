b5cd9cdb5f6c1ad3d55832e7695bfc65cf0e82e4
parsajiravand <parsajiravand@gmail.com>
2023-04-26 16:15:52 +0330

    fix initFlowbite in pages

--- app.vue
+++ app.vue
@@ -4,7 +4,16 @@
   </NuxtLayout>
 </template>
 <script setup lang="ts">
+import { initFlowbite } from "flowbite";
 import useBootstrap from "./core/useBootstrap";
 
 useBootstrap();
+const route = useRoute();
+watch(
+  () => route.path,
+  () => {
+    console.log("route changed");
+    initFlowbite();
+  }
+);
 </script>
