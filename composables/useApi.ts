193a59281c92f1bf225d785def912cd2c6f1d270
parsajiravand <parsajiravand@gmail.com>
2023-04-30 17:10:23 +0330

    add options for useApi

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,6 +1,8 @@
+
+
 export const useApi = async (endpoint: string, payload?: any) => {
   const baseUrl = useRuntimeConfig().public.baseUrl;
-  const { data, error } = await useFetch(baseUrl + endpoint);
+  const { data, error } = await useFetch(baseUrl + endpoint, payload);
   if (data.value) {
     return { data: data.value, error: null };
   } else {
